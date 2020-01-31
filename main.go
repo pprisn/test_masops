e22513ba1526e4b41da20cbb42abcb23178c030f
pprisn <pprisn@yandex.ru>
2020-01-31 21:43:46 +0300

    status

--- main.go
+++ main.go
@@ -9,9 +9,10 @@ import (
 	"flag"
 	"fmt"
 	"github.com/jinzhu/gorm"
-	"io"
+//	"io"
+	"io/ioutil"
 	"log"
-	"net"
+	"net/http"
 	"os"
 	"strings"
 	"time"
@@ -83,7 +84,7 @@ func main() {
 	//return
 	scanner := bufio.NewScanner(f)
 	var nameip string
-	d := net.Dialer{Timeout: time.Second * 4}
+//	d := net.Dialer{Timeout: time.Second * 4}
 
 	// Получить выборку
 	rows, err := db.Raw("select name from nsis").Rows()
@@ -101,13 +102,13 @@ func main() {
 		nameip = name
 		vStatus7502, vStatus7522, vStatus7504, vStatus7501 = "", "", "", ""
 		port = "7502"
-		vStatus7502 = checkStatus(d, i, nameip, port)
+		vStatus7502 = checkStatus(i, nameip, port)
 		port = "7522"
-		vStatus7522 = checkStatus(d, i, nameip, port)
+		vStatus7522 = checkStatus(i, nameip, port)
 		port = "7504"
-		vStatus7504 = checkStatus(d, i, nameip, port)
+		vStatus7504 = checkStatus(i, nameip, port)
 		port = "7501"
-		vStatus7501 = checkStatus(d, i, nameip, port)
+		vStatus7501 = checkStatus(i, nameip, port)
 
 		//db.Model(&n).Where("name = ?", nameip).Update("status", vStatus).Error()
 		db.Exec("UPDATE nsis SET updated_at=NOW(), status=? , statussdo=? , statusupd=? , statusauth=? WHERE name = ?", vStatus7502, vStatus7522, vStatus7504, vStatus7501, nameip)
@@ -122,52 +123,28 @@ func main() {
 
 }
 
-func checkStatus(d net.Dialer, i int, ip string, port string) string {
+func checkStatus( i int, ip string, port string) string {
 	fmt.Printf("%d\t%s:%s\n", i, ip, port)
-	conn, err := d.Dial("tcp", ip+".main.russianpost.ru"+":"+port)
-	if err != nil {
-		// handle error
-		log.Printf("\tError\t%s\t%s", ip, err)
-		er := fmt.Sprintf("Error Dial:[%s]{%s}", ip, err)
-		return er
-	}
-	conn.SetReadDeadline(time.Now().Add(time.Second * 10))
-	fmt.Fprintf(conn, "GET /v1/ HTTP/1.0\r\n\r\n")
-	//	jsonStatus := bufio.NewReader(conn) //.ReadString('\n')
-	buf := make([]byte, 256)
-	if _, err := io.ReadFull(conn, buf); err != nil {
+
+    client := http.Client{
+        Timeout: time.Duration(6 * time.Second),
+    }
+
+    resp, err := client.Get("http://"+ip+".main.russianpost.ru"+":"+port+"/v1")
+    if err != nil {
+	return "Error response"+":"+port
+    }
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+
+        if err != nil {
 		return "Error Read conn"
 	}
 	var status, version string = "", ""
 
-	/*
-		for {
-			line, err := jsonStatus.ReadString('\n')
-			if len(line) == 0 && err != nil {
-				if err == io.EOF {
-					break
-				}
-				break
-				//return err
-			}
-			line = strings.TrimSuffix(line, "\n")
-
-			if strings.Contains(line, `"version":`) {
-				version = line[:len(line)-1]
-
-			} else if strings.Contains(line, `"status"`) {
-				status = line[:len(line)-1]
-
-			}
-			if len(version) > 0 && len(status) > 0 {
-				break
-			}
-
-		} //
-	*/
-
 	var dat map[string]interface{}
-	if err := json.Unmarshal(buf, &dat); err != nil {
+        fmt.Printf("%s", body)
+	if err := json.Unmarshal(body, &dat); err != nil {
 		return "Error Unmarshal"
 	} else {
 
